d43d4fb6e96e2844012c4788b3ec957f25afceeb
taiga <taiga>
2023-06-13 19:30:31 +0200

    parse element data when click on details of car

--- src/Pages/Car/Car.js
+++ src/Pages/Car/Car.js
@@ -1,12 +1,15 @@
 import './Car.scss';
+import { CARS } from '../../data.js'
 import { useParams } from 'react-router-dom';
 
 function Car() {
   const params = useParams();
+  const element = CARS.find(elt => elt.id = params.id);
+  
   
   return (
     <div className="car-page-container">
-      <p>{params.id}</p>
+      <p>{element.brand}</p>
     </div>
   )
 }
---
+++ src/data.js
@@ -0,0 +1,8 @@
+export const CARS = [
+  { id: 1, brand: 'Renault', year: '2020', price: 15000, image: 'https://images.caradisiac.com/images/7/4/3/6/197436/S0-serie-d-ete-comment-va-renault-722874.jpg' },
+  { id: 2, brand: 'Toyota', year: '2023', price: 18000, image: 'https://www.shutterstock.com/image-illustration/tula-russia-june-24-2021-260nw-2013225725.jpg' },
+  { id: 3, brand: 'Ford', year: '2015', price: 25000, image: 'https://www.speedest.fr/medias/vehicules/933/img-5819.jpg' },
+  { id: 4, brand: 'Ferrari', year: '2010', price: 28000, image: 'https://images.unsplash.com/photo-1592198084033-aade902d1aae?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80' },
+  { id: 5, brand: 'Citroen', year: '2015', price: 25000, image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Citroen_e-C4_Auto_Zuerich_2021_IMG_0242.jpg/2560px-Citroen_e-C4_Auto_Zuerich_2021_IMG_0242.jpg' },
+  { id: 6, brand: 'Peugeot', year: '2010', price: 28000, image: 'https://images.caradisiac.com/images/5/5/8/2/195582/S0-peugeot-est-le-constructeur-prefere-des-francais-en-toute-subjectivite-711529.jpg' },
+]
