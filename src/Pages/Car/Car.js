ec3aa6703ea5798c083d9e6ed2cdda9db691b41b
taiga <taiga>
2023-06-14 21:07:58 +0200

    static images of car details

--- src/Pages/Car/Car.js
+++ src/Pages/Car/Car.js
@@ -5,22 +5,21 @@ import { useParams } from 'react-router-dom';
 function Car() {
   const params = useParams();
   const element = CARS.find(elt => elt.id === params.id);
+  // console.log(element.image);
+  // create an  image for every img available in the element 
+  const images = element.image.map((img, index) => {
+    return <img key={index} className='cars-image' src={img} alt='voiture'></img>
+  })
+  console.log(images);
   
   return (
     <div className="car-page-container">
       <div className='left-side center'>
         <section className='images-section center'>
-          <img className='main-image' src={element.image} alt='voiture'></img>
-          <div className='sub-image'>
-            <div className='sub-image-first-line'>
-              <img className='cars-image' src={element.image} alt='voiture'></img>
-              <img className='cars-image' src={element.image} alt='voiture'></img>
-              <img className='cars-image' src={element.image} alt='voiture'></img>
-            </div>
-            <div className='sub-image-second-line'>
-              <img className='cars-image' src={element.image} alt='voiture'></img>
-              <img className='cars-image' src={element.image} alt='voiture'></img>
-              <img className='cars-image' src={element.image} alt='voiture'></img>
+          <img className='main-image' src={element.image[0]} alt='voiture'></img>
+          <div className='sub-image-container'>
+            <div className='sub-image'> 
+              {images}
             </div>
           </div>
         </section>
